3d588eed956f3e581e3f3893b2765a52a6aaeb0b
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-09-16 22:18:02 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -14,6 +14,7 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, Invit
 from config import *
 from helper_func import *
 from database.database import *
+from database.database import db
 
 #──────────────────────
 #────────ᴊᴇғғʏ ᴅᴇᴠ─────────
